d68f91015c92791ee9bef47352f9a89b3cfec905
deanDev5200 <deanpo5200@gmail.com>
2026-07-07 12:27:55 +0800

    aws

---
+++ frontend/uninstall-service.js
@@ -0,0 +1,15 @@
+const Service = require('node-windows').Service;
+
+// Create a new service object (must match the installed service)
+const svc = new Service({
+  name: 'IPC Frontend',
+  script: require('path').join(__dirname, 'node_modules', 'serve', 'bin', 'serve.js')
+});
+
+// Listen for the "uninstall" event
+svc.on('uninstall', () => {
+  console.log('Service uninstalled successfully!');
+});
+
+// Uninstall the service
+svc.uninstall();
