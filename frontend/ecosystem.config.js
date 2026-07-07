2c2f7606c390928d7d6fade10d450c36eeb62376
deanDev5200 <deanpo5200@gmail.com>
2026-07-07 12:57:24 +0800

    hoinjl

--- frontend/ecosystem.config.js
+++ frontend/ecosystem.config.js
@@ -1,12 +1,13 @@
 module.exports = {
   apps: [{
     name: 'ipc-frontend',
-    script: 'node',
-    args: 'serve.js',
+    script: './node_modules/serve/bin/serve.js',
+    args: '-s build -l 3000',
     instances: 1,
     autorestart: true,
     watch: false,
     max_memory_restart: '1G',
+    cwd: __dirname,
     env: {
       NODE_ENV: 'production'
     }
